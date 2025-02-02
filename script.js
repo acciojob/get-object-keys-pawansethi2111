4892568bfe5d268764159230067808a91e66727b
PAWAN SETHI <66323571+pawansethi2111@users.noreply.github.com>
2025-02-02 18:31:36 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
 //your JS code here. If required.
+// Create student object
+const student = {
+    name: "John"
+};
+
+// Function to get keys of an object
+function getKeys(obj) {
+    return Object.keys(obj);
+}
+
+// Test the function
+console.log(getKeys(student)); // Output: ["name"]
